ec6194b1a7f2a3ee00385293763e50fc7162485a
Angie Lin <billyboxzoo@gmail.com>
2024-11-10 18:06:47 +0300

    update

--- src/z9.java
+++
@@ -1,9 +0,0 @@
-public class z9 {
-    public static void main(String[] args) {
-        System.out.println(whatYear(100));
-    }
-
-    public static boolean whatYear(int num) {
-        return (num % 4 == 0 && num % 100!= 0) || num % 400 == 0;
-    }
-}
